e9dc6938089eadd008416d4c2d5c1b5dd4069efd
vocaltech <it@vocaltech.fr>
2022-05-08 13:33:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rxb6vi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rxb6vi)
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css'],
+})
+export class HelloComponent implements OnInit {
+  @Input() name: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
